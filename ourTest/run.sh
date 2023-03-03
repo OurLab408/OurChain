704fd36a7f211faa075289433d8fdbfad91ce77c
pp0722 <84711608+pp0722@users.noreply.github.com>
2023-03-03 13:42:58 +0000

    bug fix

--- ourTest/run.sh
+++ ourTest/run.sh
@@ -3,21 +3,31 @@
 space=" "
 node=$1
 cmd=$2
-args=$3
-shift 3
-for i
-do
-    if [[ "$i" =~ "$space" ]]
-    then 
-        args="$args ${i@Q}"
-    else
-        args="$args $i"
-    fi
-done
 
-if [ "$#" = "2" ]
+if [ "$cmd" = "test" ]
+then
+    cmd2=$3
+    shift 1
+fi
+
+args=$3
+if [ "$#" -le 2 ]
 then
     args=""
+fi
+
+if [ "$#" -ge 3 ]
+then
+    shift 3
+    for i
+    do
+        if [[ "$i" =~ "$space" ]]
+        then 
+            args="$args ${i@Q}"
+        else
+            args="$args $i"
+        fi
+    done
 fi
 
 if [ "$cmd" = "start" ]
@@ -25,10 +35,10 @@ then
     bitcoind --datadir=node/"$node"
 elif [ "$cmd" = "test" ]
 then
-    echo "bitcoin-cli --datadir=node/$node $cmd $args"
+    echo "bitcoin-cli --datadir=node/$node $cmd2 $args"
 elif [ "$cmd" = "kill" ]
 then
     rm -rf node/"$node"/regtest
 else
-    bitcoin-cli --datadir=node/$node $cmd $args
+    echo "bitcoin-cli --datadir=node/$node $cmd $args" | bash
 fi
